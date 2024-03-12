df171849438ff8b4870d16691eba047a48311e94
xdd <xdd@xdd.com>
2024-03-12 18:02:59 -0400

    vite config base change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
  plugins: [react()],
- base: "https://kraw-codehorde.github.io/my-app/",
+ base: "https://kraw-codehorde.github.io/Lair/",
  
 });
