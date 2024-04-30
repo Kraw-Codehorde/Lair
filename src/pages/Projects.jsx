98988daf04ffdf99d937077368258c1d67d0f2ff
xdd <xdd@xdd.com>
2024-04-30 16:07:29 -0400

    added gh links rendering

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const mapIdToUrl = {
+  5: "https://github.com/Kraw-Codehorde/Lair",
+  6: "https://github.com/Kraw-Codehorde/Core_API",
+  7: "https://github.com/Kraw-Codehorde/room-pals",
+  8: "https://github.com/Kraw-Codehorde/room-pals2",
+};
+
 const Projects = () => {
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -9,11 +16,14 @@ const Projects = () => {
     const _header = {
       "Content-Type": "application/json",
       Authorization: "Api-Key cqpiFIS2.TnvxK5pp3jNNE8gzyLB1EgEtm3TgXwkF",
-      Origin: 'https://kraw-codehorde.github.io/Lair/',
+      Origin: "https://kraw-codehorde.github.io/Lair/",
     };
-    const response = await axios.get("https://api.krawcodehorde.xyz:1338/api/projects", {
-      headers: _header,
-    });
+    const response = await axios.get(
+      "https://api.krawcodehorde.xyz:1338/api/projects",
+      {
+        headers: _header,
+      }
+    );
     // console.log('response', response.data);
     setLoading(false);
     setProjects(response.data);
@@ -73,41 +83,51 @@ const Projects = () => {
             <div>...loading</div>
           ) : (
             projects.map((project) => (
-            <>
-              <div className="col-md-6" data-animate-effect="fadeInLeft">
-                <div
-                  className="project"
-                  style={{ backgroundImage: `url(${project.image})` }}
-                >
-                  <div className="desc">
-                    <div className="con">
-                      <h3>
-                        <a href={project.url}>{project.name}</a>
-                      </h3>
-                      <span>{project.description}</span>
-                      <p className="icon">
-                        <span>
-                          <a href="#">
-                            <i className="icon-share3"></i>
-                          </a>
-                        </span>
-                        <span>
-                          <a href="#">
-                            <i className="icon-eye"></i> 100
-                          </a>
-                        </span>
-                        <span>
-                          <a href="#">
-                            <i className="icon-heart"></i> 49
-                          </a>
-                        </span>
-                      </p>
+              <>
+                <div className="col-md-6" data-animate-effect="fadeInLeft">
+                  <div
+                    className="project"
+                    style={{ backgroundImage: `url(${project.image})` }}
+                  >
+                    <div className="desc">
+                      <div className="con">
+                        <h3>
+                          <a href={project.url}>{project.name}</a>
+                        </h3>
+                        <span>{project.description}</span>
+                        <p className="icon">
+                          <span>
+                            {project.id >= 5 && project.id <= 8 ? (
+                              <a href={mapIdToUrl[project.id]}>
+                                <i className="icon-share3"></i>
+                              </a>
+                            ) : (
+                              <span>
+                                <a href="#">
+                                  <i className="icon-share3"></i>
+                                </a>
+                              </span>
+                            )}
+                          </span>
+
+                          <span>
+                            <a href="#">
+                              <i className="icon-eye"></i> 100
+                            </a>
+                          </span>
+                          <span>
+                            <a href="#">
+                              <i className="icon-heart"></i> 49
+                            </a>
+                          </span>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </>)
-          ))}
+              </>
+            ))
+          )}
           {/* <div
             className="col-md-6 animate-box"
             data-animate-effect="fadeInRight"
